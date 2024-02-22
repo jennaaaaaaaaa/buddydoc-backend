0c24e07cdc3f6e98f62e36fbae70304acf07c6e1
박성진 <tjdwls9355@gmail>
2024-02-22 16:31:22 +0900

    feat : 비밀번호 암호화

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -19,32 +19,35 @@ import {
 import { Response, Request } from 'express';
 import { ApiTags, ApiOperation } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
+import {BcryptService} from '../utils/bcrypt/bcrypt.service'
 import { KakaoAuthGuard, NaverAuthGuard } from './oauth/auth.guard';
 import { GoogleAuthGuard } from './oauth/auth.guard';
 
 @ApiTags('login')
 @Controller()
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly bcryptService: BcryptService
+  ) {}
 
   /**
    * 사용자 인증 후 회원 체크
-   * @param res 
-   * @param req 
-   * @param user 
+   * @param res
+   * @param req
+   * @param user
    */
   private async checkUser(res: Response, req: Request, user: any) {
     try {
       const checkUser = await this.authService.findUser(user);
 
-      
-      
-      //delete user.password
+      //비밀번호 암호화
+      user.password = await this.bcryptService.hashPassword(user.password)
 
-      res.cookie('authCookie',user, {
+      res.cookie('authCookie', user, {
         maxAge: 900000,
-        httpOnly:true
-      })
+        httpOnly: true,
+      });
 
       if (checkUser) {
         res.redirect('/login');
@@ -103,17 +106,15 @@ export class AuthController {
 
   @Get('/signup')
   signUp(@Res() res: Response, @Req() req: Request) {
-    let test = req.cookies['authCookie']
-    console.log('쿠키확인 ',test)
+    let test = req.cookies['authCookie'];
+    console.log('쿠키확인 ', test);
     res.status(200).json({ message: '회원가입 form' });
   }
 
-  
   @Get('/login')
   async login(@Res() res: Response, @Req() req: Request) {
-    let test = req.cookies['authCookie']
-    console.log('쿠키확인' , test)
+    let test = req.cookies['authCookie'];
+    console.log('쿠키확인', test);
     res.status(200).json({ message: '로그인' });
   }
-
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,9 +5,10 @@ import { PrismaModule } from 'src/database/prisma/prisma.module';
 import { KakaoStrategy } from './oauth/kakao/kakao-strategy';
 import { GoogleStrategy } from './oauth/google/google-strategy';
 import { NaverStrategy } from './oauth/naver/naver-strategy';
+import { UtilsModule } from 'src/utils/utils.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule,UtilsModule],
   controllers: [AuthController],
   providers: [AuthService, KakaoStrategy, GoogleStrategy, NaverStrategy],
 })

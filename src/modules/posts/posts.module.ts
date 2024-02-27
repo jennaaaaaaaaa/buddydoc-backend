baa80ba1aa2f5d952a63a77d0bd9d3e2be9dbbe8
박성진 <tjdwls9355@gmail>
2024-02-28 01:55:45 +0900

    test : yml test

--- src/modules/posts/posts.module.ts
+++ src/modules/posts/posts.module.ts
@@ -3,7 +3,6 @@ import { PostController } from './posts.controller';
 import { PostService } from './posts.service';
 import { PrismaModule } from '../../database/prisma/prisma.module';
 import { S3Module } from 'src/providers/aws/s3/s3.module';
-
 import { ElasticsearchModule, ElasticsearchService } from '@nestjs/elasticsearch';
 import { SearchModule } from './search/search.module';
 

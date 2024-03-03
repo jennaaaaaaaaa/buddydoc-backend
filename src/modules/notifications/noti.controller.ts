6e8110676430ac3d14a7fb38715b1cdf9405068d
박성진 <tjdwls9355@gmail>
2024-03-03 20:13:46 +0900

    feat : 알림 구현

--- src/modules/notifications/noti.controller.ts
+++ src/modules/notifications/noti.controller.ts
@@ -39,7 +39,7 @@ export class NotiContoller {
   async createNotification(@Body() notiDto: NotiDto, @Res() res: Response, @Req() req: Request) {
     try {
       const postId = Number(req.params['post']);
-      console.log(req.user['id']);
+      console.log('유저 > ',req.user['id']);
       notiDto.noti_userId = req.user['id'];
       notiDto.postId = postId;
       //게시글 작성자 확인
@@ -49,7 +49,7 @@ export class NotiContoller {
       //await this.notiService.sendNotification(notiDto);
       
       //실시간 알림 보내기
-      this.alarmGateway.sendNotification(notiDto.noti_message , notiDto.userId);
+      this.alarmGateway.sendMessageToUser(notiDto.userId,notiDto.noti_message);
       return res.status(200).json({ message: '신청완료' });
     } catch (error) {
       throw new BadRequestException('신청에러');

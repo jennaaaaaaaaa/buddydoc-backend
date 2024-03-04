4a7fc22ca62b583fd44d23ef1cc1d6a0cbf5b8e8
박성진 <tjdwls9355@gmail>
2024-03-05 04:26:46 +0900

    feat : 알림 insert

--- src/modules/notifications/noti.module.ts
+++ src/modules/notifications/noti.module.ts
@@ -4,9 +4,10 @@ import { NotiService } from './noti.service';
 import { PrismaModule } from '../../database/prisma/prisma.module';
 import { AlarmModule } from '../alarm/alarm.module';
 import { AlarmGateway } from '../alarm/alarm.gateway';
+import { AlarmService } from '../alarm/alarm.service';
 @Module({
   imports: [PrismaModule],
   controllers: [NotiContoller],
-  providers: [NotiService,AlarmGateway],
+  providers: [NotiService,AlarmGateway,AlarmService],
 })
 export class NotiModule {}

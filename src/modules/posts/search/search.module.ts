2bb218e5ccb48dfd081fc39806ddab0b95f27056
jennaaaaaaaaa <ggm4954@gmail.com>
2024-03-21 16:59:24 +0900

    refactor: elastic cloud 만료로 인해 로컬 서버로 변경

--- src/modules/posts/search/search.module.ts
+++ src/modules/posts/search/search.module.ts
@@ -4,18 +4,37 @@ import { SearchService } from './search.service';
 import { PrismaModule } from '../../../database/prisma/prisma.module';
 import { PrismaService } from 'src/database/prisma/prisma.service';
 
+// elastic cloud 만료
+// @Module({
+//   imports: [
+//     ElasticsearchModule.registerAsync({
+//       useFactory: () => ({
+//         cloud: {
+//           id: process.env.ELASTIC_CLOUD_ID,
+//         },
+//         auth: {
+//           username: process.env.ELASTIC_USERNAME,
+//           password: process.env.ELASTIC_PASSWORD,
+//         },
+//         maxRetries: 10, //있어야되나?...
+//       }),
+//     }),
+//     PrismaModule,
+//   ],
+//   providers: [SearchService, PrismaService],
+//   exports: [SearchService],
+// })
+// export class SearchModule {}
+
 @Module({
   imports: [
     ElasticsearchModule.registerAsync({
       useFactory: () => ({
-        cloud: {
-          id: process.env.ELASTIC_CLOUD_ID,
-        },
-        auth: {
-          username: process.env.ELASTIC_USERNAME,
-          password: process.env.ELASTIC_PASSWORD,
-        },
-        maxRetries: 10, //있어야되나?...
+        node: 'http://localhost:9200',
+        maxRetries: 10,
+        requestTimeout: 60000,
+        pingTimeout: 60000,
+        sniffOnStart: true,
       }),
     }),
     PrismaModule,
